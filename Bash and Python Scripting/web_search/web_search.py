0de803d93b34717e810a10d8710c8f4c524b49e1
jittu-pal <jksuperking3@gmail.com>
2026-07-12 22:51:52 +0530

    web_search

---
+++ Bash and Python Scripting/web_search/web_search.py
@@ -0,0 +1,19 @@
+import sys
+import webbrowser
+
+def google_search(query):
+    search_query = f"{query} site:greenhouse.com OR site:lever.com OR site:lever.in after:2025"
+    
+    encoded_query = search_query.replace(" ", "+")
+    
+    url = f"https://www.google.com/search?q={encoded_query}"
+    
+    webbrowser.open(url)
+    
+if len(sys.argv) > 1:
+    search_item = ' '.join(sys.argv[1:])
+    google_search(search_item)
+else:
+    print("Please provide a search item")
+    
+    
